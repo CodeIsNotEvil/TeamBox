81ade3ee30aa8e4ceb197aa1ac0a20ff5dc553ef
CodeIsNotEvil <codeisnotevil@gmail.com>
2021-03-16 08:08:22 +0100

    filesystempath changes to the start scripts

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 #
-# /home/pi/scripts/server_start.sh
+# /home/ubuntu/scripts/server_start.sh
 #
 #
 # This scripts starts nodejs applications on ports 3000, 8000 & 9001 if they
@@ -9,13 +9,16 @@
 #
 # Author: Lara Klimm
 # Date: June 15th 2016
+#
+# Edited by Lukas Reichwein
+# Date 15.03.2021
 
 
 # start own NodeJs app on port 3000
 app=$(netstat -lt | grep "3000")
 if [ -z "$app" ]
 then
-	node /var/www/html/app/server.js &
+	node /home/ubuntu/app/server.js &
 	printf "\nStarted app"
 else
 	printf "\nApp is already running"
@@ -25,7 +28,7 @@ fi
 etherpad=$(netstat -lt | grep "9001")
 if [ -z "$etherpad" ]
 then
-	sudo -H -u pi bash -c /home/pi/etherpad-lite/bin/run.sh &
+	sudo -H -u ubuntu bash -c /home/ubuntu/etherpad-lite/bin/run.sh &
 	printf "\nStarted etherpad"
 else
 	printf "\nEtherpad is already running"
@@ -35,9 +38,9 @@ fi
 ethercalc=$(netstat -lt | grep "8000")
 if [ -z "$ethercalc" ]
 then
-	# first change directory (to save dump in pi's home)
-	cd /home/pi
-	sudo -H -u pi bash -c ethercalc &
+	# first change directory (to save dump in ubuntu's home)
+	cd /home/ubuntu
+	sudo -H -u ubuntu bash -c ethercalc &
 	printf "\nStarted ethercalc"
 else
 	printf "\nEthercalc is already running"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 #
-# /home/pi/scripts/start.sh
+# /home/ubuntu/scripts/start.sh
 #
 # This script is a start script to mount a USB storage device, create nessecary
 # data structure, choose a group (or create a new group), import the group's 
 # mysql dump & start wlan access point.
 #
 # Dependencies:
-# /home/pi/scripts/usb_mount.s
-# /home/pi/scripts/group_choose.sh
-# /home/pi/scripts/mysql_import.sh
-# /home/pi/scripts/wlan_start.sh
+# /home/ubuntu/scripts/usb_mount.s
+# /home/ubuntu/scripts/group_choose.sh
+# /home/ubuntu/scripts/mysql_import.sh
+# /home/ubuntu/scripts/wlan_start.sh
 # Indirect dependencies:
-# /etc/init.d/hostapd
-# /home/pi/scripts/group_create.sh
-# /home/pi/scripts/wlan_show_info.sh
-# /home/pi/scripts/wlan_set_random_psk.sh
-# /home/pi/scripts/wlan_set_random_ssid.sh
-# /home/pi/scripts/wlan_show_psk.sh
-# /home/pi/scripts/wlan_show_ssid.sh
-# /home/pi/scripts/wlan_show_ip.sh
+# /home/ubuntu/scripts/group_create.sh
+# /home/ubuntu/scripts/wlan_show_info.sh
+# /home/ubuntu/scripts/wlan_set_random_psk.sh
+# /home/ubuntu/scripts/wlan_set_random_ssid.sh
+# /home/ubuntu/scripts/wlan_show_psk.sh
+# /home/ubuntu/scripts/wlan_show_ssid.sh
+# /home/ubuntu/scripts/wlan_show_ip.sh
 #
 # Author: Lara Klimm
 # Date: 02.05.2016
+#
+# Edited by Lukas Reichwein
+# Date 15.03.2021
+
 
 
 exit_status=1
@@ -30,26 +33,26 @@ exit_status=1
 clear
 
 # mount connceted usb storage devices
-sudo bash /home/pi/scripts/usb_mount.sh
+sudo bash /home/ubuntu/scripts/usb_mount.sh
 exit_status="$?"
 
 if [ "$exit_status" = 0 ]
 then
 	# choose group or create new group & nessecary data structur on usb
-	# sudo bash /home/pi/scripts/group_create.sh is called from cript below, if there a no groups yet
+	# sudo bash /home/ubuntu/scripts/group_create.sh is called from cript below, if there a no groups yet
 	printf "\n=============================================================================\n\n"
-	sudo bash /home/pi/scripts/group_choose.sh
+	sudo bash /home/ubuntu/scripts/group_choose.sh
 	exit_status="$?"
 
 	if [ "$exit_status" = 0 ]
 	then
 		# import mysql dump from group
 		printf "\n=============================================================================\n\n"
-		sudo bash /home/pi/scripts/mysql_import.sh
+		sudo bash /home/ubuntu/scripts/mysql_import.sh
 		exit_status="$?"
 		printf "\n=============================================================================\n\n"
 		# start wlan access point
-		sudo bash /home/pi/scripts/wlan_start.sh
+		sudo bash /home/ubuntu/scripts/wlan_start.sh
 		exit_status="$?"
 	fi
 fi
