ecb8acc046ec8fab4e156af87b1327a592c54a74
CodeIsNotEvil <codeisnotevil@gmail.com>
2021-03-22 13:22:08 +0100

    Added MongoDB connection

--- app/server.js
+++ app/server.js
@@ -40,6 +40,29 @@ var io = require('/usr/local/lib/node_modules/socket.io')(http, {
 var cheerio = require('/usr/local/lib/node_modules/cheerio'),
         $ = cheerio.load("<container id='-1'></container>");
 var crypto = require('crypto');
+const { MongoClient } = require("mongodb");
+
+//MongoDB Wekan setup
+const uri = "mongodb://localhost:27017/wekan";
+const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 //Mysql TeamBox setup
 var mysqlConnection = mysql.createConnection({
