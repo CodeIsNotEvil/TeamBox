2fede2bb4860170fe86a6ec04beead623fda24dd
CodeIsNotEvil <codeisnotevil@gmail.com>
2021-05-10 09:34:24 +0200

    Removed unneccecary logging

--- services/sockets/ethercalcController.js
+++ services/sockets/ethercalcController.js
@@ -5,13 +5,13 @@ const fetch = require('node-fetch');
 
 module.exports.exportXlsxFileToUSB = async (fileName) => {
     let xlsxData = await fetchXlsxFormEthercalc(fileName);
-    console.log(xlsxData);
+    //console.debug(xlsxData);
     await writeXlsxDataToUSB(fileName, xlsxData);
 }
 
 
 fetchXlsxFormEthercalc = async (fileName) => {
-    const response = await fetch(`http://teambox.local:8000/_/${fileName}/xlsx`, { method: 'GET', redirect: 'follow' });
+    const response = await fetch(`http://teambox.local:8000/_/${fileName}/xlsx`);
     const buffer = await response.buffer();
     return buffer;
 }
@@ -20,7 +20,7 @@ writeXlsxDataToUSB = async (fileName, xlsxData) => {
     const group = await Group.findOne({ isActive: true });
     let pathToUSBFiles = `${group.usbPath}/files/tables`
     let pathWithFileName = `${pathToUSBFiles}/${fileName}.xlsx`;
-    console.debug("writeImageToUSBFolder >>> ", pathWithFileName);
+    //console.debug("writeImageToUSBFolder >>> ", pathWithFileName);
     if (requireFolder(pathToUSBFiles)) {
         return await writeXlsXData(pathWithFileName, xlsxData);
     }
@@ -28,8 +28,7 @@ writeXlsxDataToUSB = async (fileName, xlsxData) => {
 
 writeXlsXData = async (pathWithFileName, xlsxData) => {
     let wstream = fs.createWriteStream(pathWithFileName);
-    let error = await wstream.write(xlsxData);
-    console.log(error);
-    console.debug(`writeXlsXData >>> successfully wrote xlsxData to ${pathWithFileName}`);
+    await wstream.write(xlsxData);
+    //console.debug(`writeXlsXData >>> successfully wrote xlsxData to ${pathWithFileName}`);
     return await wstream.end();
 }
